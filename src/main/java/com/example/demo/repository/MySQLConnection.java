d0cff0f58e8774db5e4b5d21048ecdfdbddba79d
Himani Agnihotri <himani.a2395@gmail.com>
2025-09-18 12:44:33 +0530

    Added Changes

--- src/main/java/com/example/demo/repository/MySQLConnection.java
+++ src/main/java/com/example/demo/repository/MySQLConnection.java
@@ -1,12 +1,21 @@
 package com.example.demo.repository;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(prefix = "sqlconnection",value = "enabled",havingValue = "true",matchIfMissing = false)
 public class MySQLConnection {
-    public MySQLConnection() {
-        System.out.println("Initialization of MySQLConnection");
+
+    @Value("${username}")
+    String username;
+
+    @Value("${password}")
+    String password;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Username: "+ username + " " + "Password: "+ password);
     }
+
 }
