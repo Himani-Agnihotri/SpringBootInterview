b35c746f49ee752cab8c317eaf76da289a6e27c5
Himani Agnihotri <himani.a2395@gmail.com>
2025-09-28 20:54:06 +0530

    Added Changes

--- src/main/java/com/example/demo/service/AppConfig.java
+++ src/main/java/com/example/demo/service/AppConfig.java
@@ -2,12 +2,14 @@ package com.example.demo.service;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements AsyncConfigurer {
 
     @Bean(name="myThreadPoolExecutor")
     public Executor taskPoolExecutor(){
@@ -15,14 +17,21 @@ public class AppConfig {
         int maxPoolSize=4;
         int queueSize=3;
 
-        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
-        poolTaskExecutor.setCorePoolSize(minPoolSize);
-        poolTaskExecutor.setMaxPoolSize(maxPoolSize);
-        poolTaskExecutor.setQueueCapacity(queueSize);
-        poolTaskExecutor.setThreadNamePrefix("MyThread-");
-        poolTaskExecutor.initialize();
-        return poolTaskExecutor;
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(minPoolSize,maxPoolSize,
+                1, TimeUnit.HOURS,new ArrayBlockingQueue<>(queueSize),new CustomThreadFactory());
+        return poolExecutor;
 
     }
 
+}
+
+class CustomThreadFactory implements ThreadFactory{
+
+    private final AtomicInteger threadNo = new AtomicInteger(1);
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r);
+        thread.setName("MyThread-: "+ threadNo.getAndIncrement());
+        return thread;
+    }
 }
