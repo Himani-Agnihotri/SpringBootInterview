723918f6f1c9f003e48d8e59ab315d7456b950eb
Himani Agnihotri <himani.a2395@gmail.com>
2026-01-04 19:57:39 +0530

    Added Changes

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,18 +1,17 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.CustomRateLimiter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/orders")
-public class OrderController {
+@RequestMapping("/products")
+public class ProductController{
 
     @GetMapping("/{id}")
-    @CustomRateLimiter(limit = 2, windowsInSeconds = 5)
-    public String getOrders(String id){
-        return "orders";
+    public String getProduct(@PathVariable String id) throws InterruptedException {
+        Thread.sleep(5000);
+        return "Product: "+ id;
     }
-
 }
